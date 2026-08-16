765cfe3ec7eeea38937b50e14769845524d4688e
Amrit Thapa <zyklen908@gmail.com>
2026-08-16 06:32:17 +0545

    fix: resolve cors issue

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -2,8 +2,26 @@ package router
 
 import "github.com/gin-gonic/gin"
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(24)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func SetupRouter(h *TodoHandler) *gin.Engine {
 	r := gin.Default()
+
+	r.Use(CORSMiddleware())
 
 	api := r.Group("/api")
 	{
